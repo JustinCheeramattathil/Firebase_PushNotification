c59fd5f6188991b6179e11e3c6e00bd92502df5b
Justin Thomas <thomasjustin2203@gmail.com>
2023-12-13 23:16:23 +0530

    first commit

---
+++ lib/api/firebase_api.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FirebaseApi {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+
+  Future<void> initNotification()async{
+    await
+  }
+}
